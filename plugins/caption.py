b7879ab229e6c924b9e4154160d7e41d9fcdb48f
Soebb <87156166+Soebb@users.noreply.github.com>
2021-08-22 11:37:52 +0430

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -13,10 +13,11 @@ async def caption(client, message: Message):
         fa = " "
         X = " "
         tz = " "
-
+        Lo = " "
         if "Sen Cal Kapimi" in m:
             fa += "#تو_در_خانه_ام_را_بزن"
             X += "Sen Cal Kapimi"
+            Kh = m.replace("Sen Cal Kapimi", "")
         if "Marasli" in m:
             fa += "#اهل_ماراش"
             X += "Marasli"
@@ -210,12 +211,15 @@ async def caption(client, message: Message):
             elif "6" in Jn:
                 tz += "#ششم"
             if X.__contains__("a") or X.__contains__("o") or X.__contains__("i") or X.__contains__("c") or X.__contains__("b") or X.__contains__("e") or X.__contains__("l") or X.__contains__("n") or X.__contains__("m"):
-                V = m.split("Bolum")[0]
-                E = V.split(f"{X}", -1)[0]
+                Yd = X.replace(" ", "_")
+                Lo += "#" + f"{Yd}"
+                V = Kh.split("Bolum")[0]
+                E = V.replace(".", "").replace("_", " ").replace("-", " ")
             else:
                 E = ""
             Tzz = tz.replace("#", " ")
-            await message.edit(f"⬇️ تیزر{Tzz} قسمت {E} {fa} بازیرنویس چسبیده\n🆔👉 @dlmacvin_new")
+            date = "پنجشنبه ساعت 4 بامداد از رسانه اینترنتی دی ال مکوین"
+            await message.edit(f"⬇️ تیزر{Tzz} قسمت {E} ({fa} ) {Lo} ، بازیرنویس چسبیده\n\n🔻 پخش {date}\n\n🆔👉 @dlmacvin_new")
         if (media.file_size > 50) and N.__contains__("E0") or N.__contains__("E1") or N.__contains__("E2") or N.__contains__("E3") or N.__contains__("E4") or N.__contains__("E5") or N.__contains__("E6") or N.__contains__("E7") or N.__contains__("E8") or N.__contains__("E9"):
             if '720P' in m:
                 Q = '720'
