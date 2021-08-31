25072f52bdcb9c6a04c36a906b83f41db343353d
Soebb <87156166+Soebb@users.noreply.github.com>
2021-09-01 02:56:02 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,6 +30,16 @@ START_BTN = InlineKeyboardMarkup(
     )
 
 
+@Bot.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TXT.format(update.from_user.mention)
+    reply_markup = START_BTN
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
 
 @Bot.on_message(filters.channel & filters.audio)
 async def tag(bot, m):
